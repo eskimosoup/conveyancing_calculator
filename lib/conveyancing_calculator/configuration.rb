e7892d481db99184d2573e576521be968561cddb
Paul <pauloptimised@users.noreply.github.com>
2017-07-07 10:14:19 +0100

    Bump version number

--- lib/conveyancing_calculator/configuration.rb
+++ lib/conveyancing_calculator/configuration.rb
@@ -11,31 +11,39 @@ module ConveyancingCalculator
     yield(configuration)
   end
 
-
   class Configuration
     attr_accessor :vat_rate
-    attr_writer :sale_fee_bands, :purchase_fee_bands, :additional_costs
+    attr_writer :sale_fee_bands,
+                :purchase_fee_bands,
+                :additional_costs,
+                :additional_static_costs
 
     def initialize
       @vat_rate = 0.2
       @sale_fee_bands = nil
       @purchase_fee_bands = nil
       @additional_costs = nil
+      @additional_static_costs = nil
     end
 
     def sale_fee_bands
-      raise ConveyancingCalculator::ConfigurationError, "Sale fee bands have not been set" if @sale_fee_bands.nil?
+      raise ConveyancingCalculator::ConfigurationError, 'Sale fee bands have not been set' if @sale_fee_bands.nil?
       @sale_fee_bands
     end
 
     def purchase_fee_bands
-      raise ConveyancingCalculator::ConfigurationError, "Purchase fee bands have not been set" if @purchase_fee_bands.nil?
+      raise ConveyancingCalculator::ConfigurationError, 'Purchase fee bands have not been set' if @purchase_fee_bands.nil?
       @purchase_fee_bands
     end
 
     def additional_costs
-      raise ConveyancingCalculator::ConfigurationError, "Additional costs have not been set" if @additional_costs.nil?
+      raise ConveyancingCalculator::ConfigurationError, 'Additional costs have not been set' if @additional_costs.nil?
       @additional_costs
+    end
+
+    def additional_static_costs
+      raise ConveyancingCalculator::ConfigurationError, 'Additional static costs have not been set' if @additional_static_costs.nil?
+      @additional_static_costs
     end
   end
 end
